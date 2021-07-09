725321aef4b80f922ba6419591ec03b6168b99fd
AlexTrav <nerezov03@mail.ru>
2021-07-09 21:47:37 +0600

    ведется работа

--- dbSocialNetwork2.0dml.sql
+++ dbSocialNetwork2.0dml.sql
@@ -1,2 +1,22 @@
-insert ignore into users (firstname, lastname, email)
-values ('Reuben', 'Nienow', 'arlo51@example.org');
+-- 1 v
+
+insert into users (firstname, lastname, email, phone, is_deleted)
+values ('Dean', 'Satterfield', 'orion152@example.org', '9160120629', default);
+
+-- 2 v
+
+insert into users (firstname, lastname, email) 
+values
+('Petr', 'Zod', 'zod222@mail.ru'),
+('Alex', 'Mod', 'mod228@mail.ru'),
+('Vic', 'Rod', 'rod420@mail.ru');
+
+-- 3 v
+
+insert into users 
+set
+	firstname = 'Kristina',
+	lastname = 'Jast',
+	email = 'kris223@example.com',
+	phone = '9113125252'
+;
