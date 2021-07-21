3274fb2fb64288b61817c0d5dc813f82e2138c43
AlexTrav <nerezov03@mail.ru>
2021-07-21 22:58:04 +0600

    5.2 завершенно освоенно дополнительно операторы like, rlike и квантификаторы.

--- NewCommandScript.sql
+++ NewCommandScript.sql
@@ -140,9 +140,105 @@ select *
 from communities 
 where name = 'Политика';
 
+select *
+from communities
+where name like 'Политика';
 
-
-
-
+	-- Спецсимволы оператора like
 	
+	/* Символ	Описание		 
+       %        Любое колличество символов или их отсутсвие 
+       _		Ровно один символ */
 	
+	select 'Программист' like 'Программ%';
+
+	select 'Программа' like 'Программ%';
+
+	select 'Программ' like 'Программ%';
+
+	select 'Программирование' like 'П%е', 'Печенье' like 'П%е';
+	
+	select 'Программирование' like '%ние', 'Кодирование' like '%ние';
+
+	select 'Код' like '___', 'Рот' like '___', 'Абв' like '___';
+
+	select '15 %', 'my_sql';
+	
+	select '15 %' like '15 \%', 'my_sql' like 'my\_sql';
+	
+	select *
+	from communities;
+
+	select *
+	from communities
+	where name like '%ы';
+
+	select *
+	from communities
+	where name not like '%ы';
+
+	select *
+	from profiles 
+	where birthday >= '1990-01-01';
+
+	select *
+	from profiles
+	where birthday between '1990-01-01' and '2000-01-01';
+
+	select *
+	from profiles
+	where birthday like '199%';
+
+
+-- Операторы rlike и regexp
+
+select 'грамм' rlike 'грам', 'граммпластинка' rlike 'грам';
+
+	-- ^
+
+	select 'программирование' rlike '^грам', 'граммпластинка' rlike '^грамм';
+
+	-- ^ и $
+
+	select 'граммпластинка' rlike '^граммпластинка$';
+
+	select 'граммпластинка на столе' rlike '^граммпластинка$';
+
+	-- |
+
+	select 'abc' rlike 'abc|абв', 'абв' rlike 'abc|абв';
+
+	-- []
+
+	select 'a' rlike '[abc]' as a,
+		   'b' rlike '[abc]' as b,
+		   'c' rlike '[abc]' as c,
+		   'w' rlike '[abc]' as w;
+
+	select 'г' rlike '[а-яё]';
+
+	select '4' rlike '[0-9]', '7' rlike '[0123456789]';
+
+		-- Класс digit
+
+		select '7' rlike '[[:digit:]]'; 
+
+		select 'Йо' rlike '[[:digit:]]'; 
+	
+		/* -- Класс alfa
+
+		select '7' rlike '[[:alfa:]]'; 
+
+		select 'Йо' rlike '[[:alfa:]]'; */
+	
+-- Квантификаторы
+	
+select '1' rlike '^[0-9]+$' as '1',
+	   '34234' rlike '^[0-9]+$' as '34234',
+	   '342.34' rlike '^[0-9]+$' as '342.34';
+	
+select '342.34' rlike '^[0-9]*\\.[0-9]{2}$' as '342.34';	  
+		
+
+
+
